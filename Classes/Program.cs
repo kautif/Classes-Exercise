fdcf45d5f9ca9e86597805c7db869725b92f469d
Autif Kamal <kautif@gmail.com>
2023-04-03 13:32:30 -0500

    Added car class and methods

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Classes
+﻿using System.Data;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car prius = new Car();
+            prius.Make = "Toyota";
+            prius.Model = "Prius";
+            prius.Year = 2013;
+            Console.Write(prius.Make);
+            Console.Write(prius.Model);
+            Console.Write(prius.Year);
         }
     }
 }
